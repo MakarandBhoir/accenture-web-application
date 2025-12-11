30c98203c2f88ca1534c339a69d2ae4dd498232d
makarand.bhoir <makarand.bhoir@live.com>
2025-12-11 11:37:49 +0530

    setup mvc applcation

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace accenture_mvc_application.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
